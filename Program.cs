3db91b43e25c20a7364edf4103c627d5d54a202c
VoldMarius <ukusi@mail.ru>
2022-11-17 11:16:53 +0300

    treangle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Введите ширину прямоугольника:   ");
+// int width = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите высоту прямоугольника:   ");
+// int heigth = int.Parse(Console.ReadLine());
+
+// for (int i = 0; i < heigth; i++)
+// {
+//     for (int j = 0; j < width; j++)
+//     {
+//         Console.Write("#");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine();
+
+
+Console.WriteLine("Введите высоту треугольника:   ");
+int heigthT = int.Parse(Console.ReadLine());
+for (int t = 0; t <= heigthT; t++)
+{
+    for (int g = 0; g <= t; g++)
+    {
+        Console.Write("* *");
+    }
+    Console.WriteLine();
+}
+
